cfdbd8bd641eb3119d2c26f78f11bf83b266bd91
Richard Schmidt <104382590+ScRichard@users.noreply.github.com>
2026-03-16 20:37:10 +0100

    refactor: add NEXT_PUBLIC_BASE_PATH to env configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const isProduction = process.env.NODE_ENV === "production";
 const repoName = "Gymboo";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProduction ? `/${repoName}` : '',
+  },
   reactStrictMode: true,
   basePath: isProduction ? `/${repoName}` : "",
   assetPrefix: isProduction ? `/${repoName}/` : "",
